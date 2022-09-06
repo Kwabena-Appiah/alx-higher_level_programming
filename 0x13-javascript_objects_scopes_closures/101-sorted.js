bb58962c455386c25ea9c9e81852faa02d773011
Godfred Ofosu-Appiah <ofosuappiahgodfred@gmail.com>
2022-09-06 20:50:32 +0000

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
 
-const list = require('./100-data').list;
+Object.keys(dict).map(function (key, index) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+});
 
-const arr = list.map((x, i) => x * i);
-
-console.log(list);
-console.log(arr);
+console.log(newDict);
