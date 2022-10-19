2f4830d9561768dfe317315e20f1e36af6fe21b2
Godfred Ofosu-Appiah <ofosuappiahgodfred@gmail.com>
2022-10-19 04:06:56 +0000

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
+/* prints all characters of a Star Wars movie.
+    - the first argument is the Movie ID.
+    - displays one character name by line.
+*/
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) console.log(error);
+  else {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (error) console.log(error);
+        else console.log(JSON.parse(body).name);
       });
-    }
+    });
   }
 });
